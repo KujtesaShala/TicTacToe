63a3b178f4ce173f1a63d9adff0030b5da711b81
KujtesaShala <kujtesa.shala22@gmail.com>
2023-02-02 10:19:13 +0100

    player vs pc

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { main } from '@popperjs/core';
 
 interface TicTac {
   name: string;
@@ -8,6 +9,16 @@ interface TicTac {
   winner?: string | null;
   isDraw?: boolean;
 }
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]; 
 
 @Component({
   selector: 'app-board',
@@ -31,7 +42,10 @@ export class BoardComponent implements OnInit {
   closeResult = '';
   gameWinner: string = '';
 
+  
+
   @ViewChild('content', { static: true, read: TemplateRef }) content!: TemplateRef<any>;
+  randomValue: any;
   
   constructor(private modalService: NgbModal) { }
 
@@ -108,6 +122,11 @@ export class BoardComponent implements OnInit {
       this.squares.splice(index, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
+    if(this.xIsNext === false)
+    {
+      this.computersTurn();
+    }
+    
     this.winner = this.calculateWinner();
 
     let result: TicTac = {
@@ -115,14 +134,15 @@ export class BoardComponent implements OnInit {
       player1: 'X',
       player2: 'O',
     } 
-    console.log(this.winner);
-    if (this.count == 9 && !this.winner )
+   
+    if (this.count === 5 && !this.winner )
     {
       result.winner = null;
       result.isDraw = true;
       this.Draw = result.isDraw;
       this.gameEnded = true;
-      // this.open();
+     
+      // console.log(result.isDraw);
     }
     if (this.winner)
     {
@@ -130,44 +150,94 @@ export class BoardComponent implements OnInit {
       this.Draw = false;
       result.winner = this.winner;
       this.gameEnded = true;
-      // this.open();
+      console.log(this.winner);
+     
     }
     this.results.push(result)
 
     if (this.gameEnded){
-
+  
       if(this.winner === "X") {
-        
+      
         this.xScore.push('1');
         this.oScore.push('0');
+        setTimeout(() => {
+         this.newGame();
+      }, 1000);
       } 
       if(this.winner === "O") {
 
         this.xScore.push('0');
         this.oScore.push('1');
+        setTimeout(() => {
+          this.newGame();
+      }, 1000);
       }
     
       if(result.isDraw === true){
-        
+     
         this.xScore.push('-');
         this.oScore.push('-');
+        setTimeout(() => {
+          this.newGame();
+      }, 1000);
       }
     }
     // console.log(result);
   }
 
+  computersTurn(){
+
+    for(let i = 0; i< lines.length; i++) {
+      
+      const [a, b, c] = lines[i];
+
+      if(this.squares[a] && this.squares[a] === this.squares[b]) {
+        if(this.squares[c] === null){
+          // setTimeout(() => {
+            this.squares.splice(c, 1, this.player);
+            this.xIsNext = !this.xIsNext;
+        // }, 1000);
+          return;
+         }
+      }
+      if(this.squares[a] && this.squares[a] === this.squares[c]) {
+        if(this.squares[b] === null){
+          // setTimeout(() => {
+            this.squares.splice(b, 1, this.player);
+            this.xIsNext = !this.xIsNext;
+        // }, 1000);
+          return;
+         }
+      }
+      if(this.squares[b] && this.squares[b] === this.squares[c]) {
+        if(this.squares[a] === null){
+          // setTimeout(() => {
+            this.squares.splice(a, 1, this.player);
+            this.xIsNext = !this.xIsNext;
+        // }, 1000);
+          return;
+         }
+      }
+        
+    }  
+      for(let i=0; i<this.squares.length; i++) {
+
+        let randomValue = Math.floor(Math.random() * this.squares.length);
+        if(this.squares[randomValue] === null){
+          // setTimeout(() => {
+            this.squares.splice(randomValue, 1, this.player);
+            this.xIsNext = !this.xIsNext;
+        // }, 1000);
+          return;
+        }
+      } 
+  }
+ 
+
   
   calculateWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
+
     for(let i = 0; i< lines.length; i++) {
       const [a, b, c] = lines[i];
       if(
@@ -181,7 +251,7 @@ export class BoardComponent implements OnInit {
   }
 
   open() {
-    console.log(this.content)
+    // console.log(this.content)
 		this.modalService.open(this.content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
 			(result) => {
 				this.closeResult = `Closed with: ${result}`;
